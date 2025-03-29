da3ea6db46c736af4580286907a577410b725d86
suraj <surajtandel01@gmail.com>
2025-03-29 14:25:17 +0530

    remove console log

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,6 +1,5 @@
 export function add(numbers: string): number {
   if (!numbers) return 0;
-  console.log(numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0));
   return numbers
     .split(/[\n,]/)
     .map(Number)
