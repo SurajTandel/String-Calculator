3674c3a22c17d8bf7626358cdc55d9008f27ba28
suraj <surajtandel01@gmail.com>
2025-03-29 14:17:19 +0530

    pass returns sum of two comma-separated numbers

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -3,3 +3,11 @@ import { add } from "../src/stringCalculator";
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns the number itself if one number is given", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns sum of two comma-separated numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
